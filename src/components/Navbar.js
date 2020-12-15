aa62dc569789f41ed989eb408221dfad0cacd49d
irudenko86 <76029076+irudenko86@users.noreply.github.com>
2020-12-15 07:29:02 +0200

    add reactChat

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+import {NavLink} from 'react-router-dom';
+
+export const Navbar = () => (
+    <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+        <div className="navbar-brand">
+            Chat App
+        </div>
+
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <NavLink
+                    className="nav-link"
+                    to="/"
+                    exact
+                >Главная страница - чат/ToDo
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink
+                    className="nav-link"
+                    to="/about"
+                >Что я использовал и релизовал 
+                </NavLink>
+            </li>
+        </ul>
+
+    </nav>
+)
